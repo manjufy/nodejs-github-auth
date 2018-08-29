e3ec365b3c652fd6eedc1dd71910a61959634594
Manjunath Reddy <manju16832003@gmail.com>
2018-08-30 01:32:04 +0800

    Call github

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const jwks = require('jwks-rsa')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const express = require('express')
+const axios = require('axios')
 const app = express()
 
 app.use(bodyParser.json());
@@ -12,6 +13,28 @@ app.use(cors());
 // endpoints
 app.get('/', (req, res) => {
   res.json([{}])
+})
+
+app.get('/auth', (req, res) => {
+  const code = req.query.code
+  console.log('CODE', code)
+      // TODO in progress.
+      // Make a post request to Github
+    axios.post('https://github.com/login/oauth/access_token', {
+      client_id: '13713e448956673736bb',
+      client_secret: 'c1b575cce6f3fc4e77ec829c6bcd7657ebd6a9ce',
+      code,
+      accept: 'json',
+      crossDomain: true
+    }, {
+      headers: {
+        'crossDomain': true
+      }
+    })
+    .then(data => {
+      console.log('Data', data)
+    })
+
 })
 
 app.listen(3333)
